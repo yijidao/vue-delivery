0b7c89044efc69b4a7e9f1babbdbb328a8f5032f
yijidao <2815451505@qq.com>
2018-07-02 22:07:56 +0800

    使用 vue-cli 搭建了开发环境，实现了 header 组件

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      i am goods
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+//   method: console.log('111')
+};
+</script>
+
+<style scoped lang="stylus">
+</style>
